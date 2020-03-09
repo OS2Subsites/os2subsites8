15ba2bfec1740289fb8490144e399ae82b25fe68
Andriy Iun <andriy.yun@gmail.com>
2020-03-09 15:43:22 +0100

    Added FDS js to theme

--- web/themes/custom/fds_custom_theme/src/javascripts/app.js
+++ web/themes/custom/fds_custom_theme/src/javascripts/app.js
@@ -1,3 +1,10 @@
 jQuery(function ($) {
   'use strict';
 });
+
+// Initiating fælles design system js.
+// @See https://designsystem.dk/komigang/tiludviklere/
+document.addEventListener("DOMContentLoaded", function(){
+  // Handler when the DOM is fully loaded
+  DKFDS.init();
+});
