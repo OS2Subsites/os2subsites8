5f408d83d220eeef7953dd0d3394a5365ef0a601
Andriy Iun <andriy.yun@gmail.com>
2020-03-09 16:37:28 +0100

    Added settings to show hide footer logo

--- web/themes/custom/fds_custom_theme/theme-settings.php
+++ web/themes/custom/fds_custom_theme/theme-settings.php
@@ -5,4 +5,11 @@ function fds_custom_theme_form_system_theme_settings_alter(
   Drupal\Core\Form\FormStateInterface $form_state
 ) {
   $form['social']['#access'] = FALSE;
+
+  // Show footer logo.
+  $form['logo']['show_footer_logo'] = [
+    '#type' => 'checkbox',
+    '#title' => t('Show default footer logo.'),
+    '#default_value' => theme_get_setting('show_footer_logo'),
+  ];
 }
