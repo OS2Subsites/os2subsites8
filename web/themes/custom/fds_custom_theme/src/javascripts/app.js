970fc00ad200ffd8e712735b6644e9c754e38d63
Morten Nissen <mail@hjemmesidekongen.dk>
2020-04-20 13:05:09 +0200

    Move DKFDS init to base thene

--- web/themes/custom/fds_custom_theme/src/javascripts/app.js
+++ web/themes/custom/fds_custom_theme/src/javascripts/app.js
@@ -1,10 +1,3 @@
 jQuery(function ($) {
   'use strict';
 });
-
-// Initiating fælles design system js.
-// @See https://designsystem.dk/komigang/tiludviklere/
-document.addEventListener("DOMContentLoaded", function(){
-  // Handler when the DOM is fully loaded
-  DKFDS.init();
-});
