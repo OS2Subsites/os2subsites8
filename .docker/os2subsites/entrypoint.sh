fc4e7301fe69c0d58771c3f2bd28527f8a04e3e0
Andriy Iun <andriy.yun@gmail.com>
2021-04-21 13:56:57 +0200

    Added ability to use web/sites folder as volume. Updated docker image and entrypoint respectively

--- .docker/os2subsites/entrypoint.sh
+++ .docker/os2subsites/entrypoint.sh
@@ -3,4 +3,15 @@
 # Starting cron service.
 service cron start
 
+if [[ ! -f /opt/drupal/web/sites/default/settings.php ]]; then
+    echo "Adding sites/default/settings.php file"
+    mkdir -p /opt/drupal/web/sites/default/
+    mkdir -p /opt/drupal/web/sites/default/files
+    cp /opt/drupal/.docker/os2subsites/drupal/settings.php /opt/drupal/web/sites/default/settings.php
+    chown www-data:www-data -R /opt/drupal/web/sites
+    chmod g+s -R www-data:www-data -R /opt/drupal/web/sites
+    chmod 755 /opt/drupal/web/sites/default
+    chmod 644 /opt/drupal/web/sites/default/settings.php
+fi
+
 exec "$@"
