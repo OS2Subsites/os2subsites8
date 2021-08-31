adde5c382fed75ed1bb0d9616b579e710c31570f
Morten Nissen <mail@hjemmesidekongen.dk>
2021-08-31 08:01:04 +0200

    Paragraph with image prepared for usage with icon

--- web/themes/custom/fds_fredericia_theme/theme-settings.php
+++ web/themes/custom/fds_fredericia_theme/theme-settings.php
@@ -10,9 +10,14 @@ function fds_fredericia_theme_form_system_theme_settings_alter(
     return;
   }
 
+  $form['footer']['footer_show_latest_content_header'] = [
+    '#prefix' => '<h3>',
+    '#markup' => t('Latest content section'),
+    '#suffix' => '</h3>',
+  ];
   $form['footer']['footer_show_latest_content'] = [
     '#type' => 'checkbox',
-    '#title' => t('Show latest content section'),
+    '#title' => t('enable '),
     '#default_value' => theme_get_setting('footer_show_latest_content'),
   ];
 }
